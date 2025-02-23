0d9da37b5a202ceb0b790c5b00d902923b0a6213
Christoph Völker <christov@gmx.de>
2025-02-23 16:58:04 +0100

    import contacts

---
+++ import_contacts.py
@@ -0,0 +1,77 @@
+import json
+from joinbackend_app.models import Contact  # Ersetze 'your_app' mit dem Namen deiner Django-App
+
+# JSON-Daten
+contacts = [
+    {
+        "firstName": "Anton",
+        "lastName": "Mayer",
+        "email": "antom@gmail.com",
+        "phoneNumber": "235325325",
+        "profileColor": "#FF7A00",
+    },
+    {
+        "firstName": "Anja",
+        "lastName": "Schulz",
+        "email": "anjaschulz@gmail.com",
+        "phoneNumber": "235325325",
+        "profileColor": "#FFC700",
+    },
+    {
+        "firstName": "Benedikt",
+        "lastName": "Ziegler",
+        "email": "ziegler@gmx.com",
+        "phoneNumber": "235325325",
+        "profileColor": "#9327FF",
+    },
+    {
+        "firstName": "David",
+        "lastName": "Eisenberg",
+        "email": "eisenberg@googlemail.com",
+        "phoneNumber": "235325325",
+        "profileColor": "#6E52FF",
+    },
+    {
+        "firstName": "Eva",
+        "lastName": "Fischer",
+        "email": "fischer_eva@gmail.com",
+        "phoneNumber": "235325325",
+        "profileColor": "#FC71FF",
+    },
+    {
+        "firstName": "Emanuel",
+        "lastName": "Mauer",
+        "email": "e.mauer@gmail.com",
+        "phoneNumber": "235325325",
+        "profileColor": "#6E52FF",
+    },
+    {
+        "firstName": "Tatjana",
+        "lastName": "Wolf",
+        "email": "wolf@gmail.com",
+        "phoneNumber": "+49 2 2 2222 222 2",
+        "profileColor": "#FF7A00",
+    },
+    {
+        "firstName": "Marcel",
+        "lastName": "Bauer",
+        "email": "bauer@gmail.com",
+        "phoneNumber": "+49 2 2 2222 222 2",
+        "profileColor": "#1FD7C1",
+    },
+]
+
+# Kontakte in die Datenbank speichern
+def import_contacts():
+    for contact in contacts:
+        Contact.objects.create(
+            first_name=contact["firstName"],
+            last_name=contact["lastName"],
+            email=contact.get("email", "default@gmx.de"),  # Default-Wert, falls kein E-Mail vorhanden ist
+            phone_number=contact.get("phoneNumber", ""),  # Falls keine Telefonnummer vorhanden ist
+            profileColor=contact.get("profileColor", "#1FD7C1"),
+        )
+    print("Import abgeschlossen!")
+
+# Funktion ausführen
+import_contacts()
