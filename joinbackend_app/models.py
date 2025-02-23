c00b9026db21b4925708355bff2e1c2f29233131
Christoph Völker <christov@gmx.de>
2025-02-23 14:50:29 +0100

    models

--- joinbackend_app/models.py
+++ joinbackend_app/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here. (users, contacts, tasks)
-    #email
-    #models.PhoneNumberField(_(""))
-    #profile_color
 
 class Contact(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254, default="default@gmx.de")
+    phone_number = PhoneNumberField(blank=True)
+    profileColor = models.CharField(max_length=30, default="#1FD7C1")
 
 class Task(models.Model):
     task_name = models.CharField(max_length=30)
 
 class User(models.Model):
-    email = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254, default="default@gmx.de")
     password = models.CharField(max_length=30)
