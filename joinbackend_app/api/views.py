2476590f340d7d4d4b6590559f5c380a26366e29
Christoph Völker <christov@gmx.de>
2025-02-24 20:29:35 +0100

    update

--- joinbackend_app/api/views.py
+++ joinbackend_app/api/views.py
@@ -1,10 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def contacts_view(request):
-    return Response({"message": "hallo!!!"})
+    if request.method == 'GET':
+        return Response({"message": "hallo!!!"})
+    if request.method == 'POST':
+        try:
+            msg = request.data['message']
+            return Response({"message": msg}, status=status.HTTP_201_CREATED)
+        except:
+            return Response({"message": "error"}, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
